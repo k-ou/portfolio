e025ab2458c2177d05944db6f2d02bf5049b3cad
k-ou <kellio94@ucla.edu>
2016-06-02 18:03:46 -0700

    Cropped images into thumbnails/previews

--- Posts.js
+++ Posts.js
@@ -17,6 +17,14 @@ class Piece {
         `;
         const piece = document.createElement('div');
         piece.innerHTML = template;
+        var img = piece.getElementsByTagName('img')[0];
+        if (img.height > img.width) {
+            img.style.height = 'auto';
+            img.style.width = 'var(--post-width)';
+        } else {
+            img.style.height = 'var(--post-width)';
+            img.style.width = 'auto';
+        }
         return piece;
     }
 }
