538c61b210799b96de3d7cda4d642993164592f5
k-ou <kellio94@ucla.edu>
2016-06-03 21:54:12 -0700

    images resize onoad

--- Piece.js
+++ Piece.js
@@ -18,13 +18,17 @@ class Piece {
         piece.className = 'post';
         piece.innerHTML = template;
         var img = piece.getElementsByTagName('img')[0];
-        if (img.height > img.width) {
-            img.style.height = 'auto';
-            img.style.width = 'var(--post-width)';
-        } else {
-            img.style.height = 'var(--post-width)';
-            img.style.width = 'auto';
+
+        img.onload = () => {
+            if (img.height > img.width) {
+                img.style.height = 'auto';
+                img.style.width = 'var(--post-width)';
+            } else {
+                img.style.height = 'var(--post-width)';
+                img.style.width = 'auto';
+            }
         }
+
         return piece;
     }
 }
