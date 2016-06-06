0835ea335e9db09ddad8b0eabab9ec405a352cb3
k-ou <kellio94@ucla.edu>
2016-06-05 19:34:43 -0700

    Enabled sticky footer, responsive nav bar, resizes to smaller screens

--- Scene.js
+++ Scene.js
@@ -24,11 +24,37 @@ class Scene {
         }
     }
 
-    render() {
-        this.pieces.forEach(piece => {
-            const element = piece.render();
-            element.onclick = this.toggleSelectedPiece.bind(this, piece);
-            this.context.appendChild(element);
-        });
+    //    Renders art pieces based on tag selected (i.e. illustrator, traditional)
+    render(tag) {
+        removePieces();
+        if (tag === 'about') {
+            this.about();
+        }
+        this.pieces
+            .filter(_ => tag === undefined || _.tags.includes(tag))
+            .forEach(piece => {
+                const element = piece.render();
+                element.onclick = this.toggleSelectedPiece.bind(this, piece);
+                this.context.appendChild(element);
+            });
     }
+
+    //     Fills in "About" section.
+    about() {
+        var div = document.createElement('div');
+        div.id = "introdiv";
+        this.context.appendChild(div);
+
+        var p = document.createElement('p');
+        p.id = 'intro';
+        p.innerHTML = 'HEY THERE. I recently graduated from UCLA (Class of \'16, what up) with a degree in mathematics of computation. Bascially, this means that I learned a lot of math and learned a bit of code.';
+        div.appendChild(p);
+    }
+
+    //     Toggles view of portfolio dropdown menu.
+    dropdown() {
+        document.getElementById("mydrop").classList.toggle("show");
+    }
+
+
 }
--- script.js
+++ script.js
@@ -2,187 +2,43 @@
  * Created by lowellbander on 6/1/16.
  */
 
-var pieces = [
-new Piece({
-        title: 'Charcoal_Still_01',
-        description: 'My first time using charcoal. Subject is an amalgamation of still life objects.',
-        photoURL: 'Charcoal_Still_01.jpg'
-    }),
-new Piece({
-        title: 'Charcoal_Still_02',
-        description: 'a description for Charcoal_Still_02',
-        photoURL: 'Charcoal_Still_02.jpg'
-    }),
-new Piece({
-        title: 'Concept_CarbCat_01',
-        description: 'a description for Concept_CarbCat_01',
-        photoURL: 'Concept_CarbCat_01.jpg'
-    }),
-new Piece({
-        title: 'Concept_Heroes_01',
-        description: 'Conception of two heroes based on myself and a companion. First time using a bamboo pen and black India ink.',
-        photoURL: 'Concept_Heroes_01.jpg'
-    }),
-new Piece({
-        title: 'Contour_Bike',
-        description: 'a description for Contour_Bike',
-        photoURL: 'Contour_Bike.jpg'
-    }),
-new Piece({
-        title: 'Contour_Half_Blind',
-        description: 'Contour of trees drawn, followed by a blind contour of the buildings in the distance.',
-        photoURL: 'Contour_Half_Blind.jpg'
-    }),
-new Piece({
-        title: 'Contour_Portrait_01',
-        description: 'a description for Contour_Portrait_01',
-        photoURL: 'Contour_Portrait_01.jpg'
-    }),
-new Piece({
-        title: 'Figure_01',
-        description: 'a description for Figure_01',
-        photoURL: 'Figure_01.jpg'
-    }),
-new Piece({
-        title: 'Figure_02',
-        description: 'a description for Figure_02',
-        photoURL: 'Figure_02.jpg'
-    }),
-new Piece({
-        title: 'Figure_03',
-        description: 'a description for Figure_03',
-        photoURL: 'Figure_03.jpg'
-    }),
-new Piece({
-        title: 'Figure_04',
-        description: 'a description for Figure_04',
-        photoURL: 'Figure_04.jpg'
-    }),
-new Piece({
-        title: 'Figure_05',
-        description: 'a description for Figure_05',
-        photoURL: 'Figure_05.jpg'
-    }),
-new Piece({
-        title: 'Figure_06',
-        description: 'a description for Figure_06',
-        photoURL: 'Figure_06.jpg'
-    }),
-new Piece({
-        title: 'Figure_07',
-        description: 'a description for Figure_07',
-        photoURL: 'Figure_07.jpg'
-    }),
-new Piece({
-        title: 'Figure_08',
-        description: 'a description for Figure_08',
-        photoURL: 'Figure_08.jpg'
-    }),
-new Piece({
-        title: 'Figure_09',
-        description: 'a description for Figure_09',
-        photoURL: 'Figure_09.jpg'
-    }),
-new Piece({
-        title: 'Figure_10',
-        description: 'a description for Figure_10',
-        photoURL: 'Figure_10.jpg'
-    }),
-new Piece({
-        title: 'Figure_11',
-        description: 'a description for Figure_11',
-        photoURL: 'Figure_11.jpg'
-    }),
-new Piece({
-        title: 'Figure_12',
-        description: 'a description for Figure_12',
-        photoURL: 'Figure_12.jpg'
-    }),
-new Piece({
-        title: 'Figure_13',
-        description: 'a description for Figure_13',
-        photoURL: 'Figure_13.jpg'
-    }),
-new Piece({
-        title: 'Figure_14',
-        description: 'a description for Figure_14',
-        photoURL: 'Figure_14.jpg'
-    }),
-new Piece({
-        title: 'Figure_15',
-        description: 'a description for Figure_15',
-        photoURL: 'Figure_15.jpg'
-    }),
-new Piece({
-        title: 'Figure_16',
-        description: 'a description for Figure_16',
-        photoURL: 'Figure_16.jpg'
-    }),
-new Piece({
-        title: 'Figure_17',
-        description: 'a description for Figure_17',
-        photoURL: 'Figure_17.jpg'
-    }),
-new Piece({
-        title: 'Figure_18',
-        description: 'a description for Figure_18',
-        photoURL: 'Figure_18.jpg'
-    }),
-new Piece({
-        title: 'Figure_19',
-        description: 'a description for Figure_19',
-        photoURL: 'Figure_19.jpg'
-    }),
-new Piece({
-        title: 'Ink_Portrait_01',
-        description: 'a description for Ink_Portrait_01',
-        photoURL: 'Ink_Portrait_01.jpg'
-    }),
-new Piece({
-        title: 'Ink_Portrait_02',
-        description: 'a description for Ink_Portrait_02',
-        photoURL: 'Ink_Portrait_02.jpg'
-    }),
-new Piece({
-        title: 'Ink_Still_01',
-        description: 'a description for Ink_Still_01',
-        photoURL: 'Ink_Still_01.jpg'
-    }),
-new Piece({
-        title: 'Ink_Still_02',
-        description: 'a description for Ink_Still_02',
-        photoURL: 'Ink_Still_02.jpg'
-    }),
-new Piece({
-        title: 'Ink_Still_03',
-        description: 'a description for Ink_Still_03',
-        photoURL: 'Ink_Still_03.jpg'
-    }),
-new Piece({
-        title: 'Perspective_01',
-        description: 'a description for Perspective_01',
-        photoURL: 'Perspective_01.jpg'
-    }),
-new Piece({
-        title: 'Perspective_02',
-        description: 'a description for Perspective_02',
-        photoURL: 'Perspective_02.jpg'
-    }),
-new Piece({
-        title: 'SelfPortrait_01',
-        description: 'a description for SelfPortrait_01',
-        photoURL: 'SelfPortrait_01.jpg'
-    }),
-new Piece({
-        title: 'Still_Lactaid',
-        description: 'a description for Still_Lactaid',
-        photoURL: 'Still_Lactaid.jpg'
-    })];
+var TAG = Piece.getTags();
 
 var context = document.querySelector('#content');
 var scene = new Scene({
-    pieces: pieces,
+    pieces: Piece.getPieces(),
     context: context
 });
-scene.render();
+
+function removePieces() {
+    const selectedPiece = document.getElementsByClassName('Piece');
+    const content = document.getElementById('content');
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+}
+
+var htmlStyles = window.getComputedStyle(document.body);
+var headerHeight = htmlStyles.getPropertyValue('--header-height');
+var navHeight = htmlStyles.getPropertyValue('--nav-height');
+var footerHeight = htmlStyles.getPropertyValue('--footer-height');
+
+document.getElementById('content').style.height = window.innerHeight - parseInt(headerHeight) - parseInt(navHeight) - parseInt(footerHeight) + "px";
+
+//document.getElementById('content').style.height = window.innerHeight - 365 + "px";
+
 //Header.render();
+
+//     Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdown') && !event.target.matches('.dropdown-content')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
